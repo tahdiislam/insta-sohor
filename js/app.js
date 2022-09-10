3d3b3e78c91e31253217d8828f6313f9b40ddf1c
tahdi-islam <tshdi60@gmail.com>
2022-09-10 07:21:25 +0600

    solve 5th problem

--- js/app.js
+++ js/app.js
@@ -36,13 +36,18 @@ const switchTab = (id) => {
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
-        document.getElementById( "liked" ).style.display = "block";
+        const liked = document.getElementById('liked');
+        // cear priviousli added liked post
+        liked.textContent = '';
+        liked.style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
 
         displayLikedPosts();
     } else {
-        document.getElementById( "reported" ).style.display = "block";
+        const reported = document.getElementById( "reported" );
+        reported.innerHTML = '';
+        reported.style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
 
